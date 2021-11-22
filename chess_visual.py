90bb5b348727feb3b64d6ea9e98334641e996250
VorobevVictor1 <vorobev.viu@phystech.edu>
2021-11-22 18:22:52 +0300

    Chess_piece

---
+++ chess_visual.py
@@ -0,0 +1,11 @@
+
+
+class Chess_piece:
+    def __init__(self, type: str , color: str, position: list):
+        self.dead = False
+        self.type = type
+        self.color = color
+        self.position = position
+
+
+
