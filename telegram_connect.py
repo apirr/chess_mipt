b6aa08435f2ba03e8b211aa622823b1293b3c7e8
mishabar410 <mb050574@gmail.com>
2021-12-18 19:55:29 +0300

    raw changes

--- telegram_connect.py
+++ telegram_connect.py
@@ -7,28 +7,44 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(content_types=['text'])
 
-class simple_bot(bot):
-    def get_message(message):
-        message.text =
 
-
-def get_text_messages(message):
-    if message.text == "Привет":
-        bot.send_message(message.from_user.id, "Здесь можно словить хихи-хаха")
-        photo = open('/Downloads/Figure_1.png', 'rb')
-        bot.send_photo(message.from_user.id, photo)
-    elif message.text == "/help":
-        bot.send_message(message.from_user.id, "напиши число от 1 до 3 и получишь юмореску")
-    elif message.text == "1":
-        bot.send_message(message.from_user.id,
-                        "Сидят в купе поезда два хохла, пьют водку, ведут разговоры на разные философские темы. ")
-    elif message.text == "2":
-        bot.send_message(message.from_user.id,
-                        "Папа с сыном пошли однажды на горы, и сын, ударившись о камень, крикнул: ")
-    elif message.text == "3":
-        bot.send_message(message.from_user.id,
-                        "Подходит карлик к Макаревичу:")
+def get_start(message):
+    if message.text == "Начать игру":
+        bot.send_message(message.from_user.id, "Выберите тип доски:text или image")
+        bot.register_next_step_handler(message, get_board_type)
     else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+        bot.send_message(message.from_user.id, "Я тебя не понимаю, напиши Начать игру")
+
+def get_board_type(message):
+    if message.text == "text":
+        bot.send_message(message.from_user.id, "Напишите через пробел или тире ход который вы хотите сделать(используйте маленькие буквы)")
+        bot.register_next_step_handler(message, get_move)
+        return("text")
+    if message.text == "image":
+        bot.send_message(message.from_user.id, "Напишите через пробел или тире ход который вы хотите сделать(используйте маленькие буквы)")
+        bot.register_next_step_handler(message, get_move)
+        return("image")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю, попробуй еще раз")
+        bot.register_next_step_handler(message, board_type)
+
+
+def get_move(message):
+    if (message.text[0] >= "a" and message.text[0] <= "h"):
+        if (message.text[1] >= "1" and message.text[1] <= "8"):
+            if (message.text[2] == " " or message.text[2] == "-"):
+                if (message.text[3] >= "a" and message.text[3] <= "h"):
+                    if (message.text[1] >= "1" and message.text[1] <= "8"):
+                        bot.send_message(message.from_user.id, "Принято") 
+                        bot.register_next_step_handler(message, wait_for_move)
+                        return message.text
+                    else:
+                        bot.send_message(message.from_user.id, "Я тебя не понимаю, попробуй написать ход еще раз")     
+                        bot.register_next_step_handler(message, get_move)
+
+def wait_for_move(message):
+    if message.text:
+        bot.send_message(message.from_user.id, "Ждите пока сходит ваш соперник")
+        bot.register_next_step_handler(message, wait_for_move)
 
 bot.polling(none_stop=True, interval=0)
